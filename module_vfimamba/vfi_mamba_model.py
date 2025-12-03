20c5cdfee203799fcbf3065120a511f72e3fbdd0
rmatif <rmatif@proton.me>
2025-12-03 04:10:24 +0000

    fix vfi_scale

--- module_vfimamba/vfi_mamba_model.py
+++ module_vfimamba/vfi_mamba_model.py
@@ -1,5 +1,3 @@
-import math
-
 import torch
 import tqdm
 from comfy.utils import ProgressBar
@@ -45,24 +43,14 @@ def run_vfi_mamba(
         padder = InputPadder(frame_a.shape, divisor=32)
         I0_, I2_ = padder.pad(frame_a, frame_b)
 
-        if vfi_scale == 2:
-            out = model.inference(I0_, I2_, True, TTA=TTA, fast_TTA=TTA, scale=scale)
+        out_frames.append(frame_a)
+
+        for j in range(1, vfi_scale):
+            timestep = j / vfi_scale
+            out = model.inference(I0_, I2_, True, TTA=TTA, fast_TTA=TTA, scale=scale, timestep=timestep)
             mid_frame = padder.unpad(out)
-            out_frames.append(frame_a)
             out_frames.append(mid_frame)
-        else:
-            frames = _recursive_generator(
-                model=model,
-                TTA=TTA,
-                scale=scale,
-                frame1=I0_,
-                frame2=I2_,
-                num_recursions=int(math.log2(vfi_scale)),
-                index=vfi_scale // 2,
-            )
-            frames = sorted(frames, key=lambda x: x[1])
-            for pred, _ in frames:
-                out_frames.append(padder.unpad(pred))
+
         pbar.update_absolute(i, src_video.shape[0])
 
     out_frames.append(src_video[-1].unsqueeze(0))
@@ -70,20 +58,3 @@ def run_vfi_mamba(
     out_tensor = torch.cat(out_frames, dim=0).permute(0, 2, 3, 1)
     print(f"{out_tensor.shape=}")
     return out_tensor
-
-
-def _recursive_generator(
-    model, TTA, scale, frame1: torch.Tensor, frame2: torch.Tensor, num_recursions, index
-):
-    if num_recursions == 0:
-        return [(frame1, index)]
-    else:
-        mid_frame = model.inference(
-            frame1, frame2, True, TTA=TTA, fast_TTA=TTA, scale=scale
-        )
-        id = 2 ** (num_recursions - 1)
-        return _recursive_generator(
-            model, TTA, scale, frame1, mid_frame, num_recursions - 1, index - id
-        ) + _recursive_generator(
-            model, TTA, scale, mid_frame, frame2, num_recursions - 1, index + id
-        )
